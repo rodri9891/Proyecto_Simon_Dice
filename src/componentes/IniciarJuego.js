fccf750a646a393106ecb922a9cbe2fbccc2563b
rodri9891 <rodri.9891@hotmail.com>
2019-07-26 22:00:02 -0300

    prox cambio

--- src/componentes/IniciarJuego.js
+++ src/componentes/IniciarJuego.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import store from '../store';
 
+//2 array - maquina y jugador
+
+//
+numeroRandom(){
+    
+}
+
+numeroJugador(){
+    
+}
 
 export default class IniciarJuego extends React.Component{
 
